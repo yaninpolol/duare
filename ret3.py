ab6124162068d0cf9c1dc83d87a5049f6556d705
yaninpolol <yaninpolol@gmail.com>
2025-05-31 14:15:49 +0700

    Update ret3.py

--- ret3.py
+++ ret3.py
@@ -31,7 +31,7 @@ chrome_options.add_argument("--disable-domain-reliability")  # Nonaktifkan keand
 
 driver = webdriver.Chrome(seleniumwire_options=options, options=chrome_options)
 
-driver.get("https://sepolia-faucet.pk910.de/#/mine/708a87ca-92d1-468a-b0ac-9d8052698c06")
+driver.get("https://sepolia-faucet.pk910.de/#/mine/37228e9c-b860-4536-bdac-f4d63d9c02aa")
 time.sleep(50000)
 
 div_element = driver.find_element(By.CLASS_NAME, "col-3")
