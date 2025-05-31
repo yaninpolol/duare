a59c51d48571e2b96fdd8751d81033eec8eb6dbf
yaninpolol <yaninpolol@gmail.com>
2025-05-31 14:33:03 +0700

    Update ret3.py

--- ret3.py
+++ ret3.py
@@ -5,8 +5,8 @@ import time
 
 options = {
     'proxy': {
-        'http': 'http://8ab4e23e37d62bc26e35__cr.jm:3efeffa0d3f66c71@gw.dataimpulse.com:10203',
-        'https': 'https://8ab4e23e37d62bc26e35__cr.jm:3efeffa0d3f66c71@gw.dataimpulse.com:10203',
+        'http': 'http://8ab4e23e37d62bc26e35__cr.ar:3efeffa0d3f66c71@gw.dataimpulse.com:10203',
+        'https': 'https://8ab4e23e37d62bc26e35__cr.ar:3efeffa0d3f66c71@gw.dataimpulse.com:10203',
         'no_proxy': 'localhost,127.0.0.1'
     }
 }
